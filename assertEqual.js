18da4f0399d21ab4881275e795aa02a2d8ae1b11
Milton Vandol Goh <miltonvandol@gmail.com>
2022-03-24 15:10:20 -0700

    implement assertEqual

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(" Assertion Passed: " + actual + " === " + expected + "✅✅✅");
+  } else {
+    console.log(" Assertion Failed: " + actual + " !== " + expected + "🛑🛑🛑");
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
