23a28e1cce2c75a0463d7321dd52534a28e65aa3
Milton Vandol Goh <miltonvandol@gmail.com>
2022-04-12 21:21:29 -0700

    refactor using Module Export

--- head.js
+++ head.js
@@ -1,16 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected} ✅✅✅`);
-  } else {
-    console.log(`ssertion Failed: ${actual} !== ${expected} 🛑🛑🛑`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 const head = (array) => {
   return array[0];
 }
 
-
-
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello")
+module.exports = head
 
