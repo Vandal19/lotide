7ad5b4e3442a4ea89b483fe734bb7a927c79bce5
Milton Vandol Goh <miltonvandol@gmail.com>
2022-04-12 21:35:50 -0700

    Refactor using Module Exports

--- tail.js
+++ tail.js
@@ -1,17 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected} ✅✅✅`);
-  } else {
-    console.log(`ssertion Failed: ${actual} !== ${expected} 🛑🛑🛑`);
-  }
-};
+const assertEqual = require('./assertEqual');
+
 const tail = (array) => {
   let newArray = array.slice(1);
   return newArray;
 };
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
+module.exports = tail
