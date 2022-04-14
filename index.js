13910bde80fda55010694a0a4d55b1458d930bd8
Milton Vandol Goh <miltonvandol@gmail.com>
2022-04-13 21:50:07 -0700

    index of all lotide

---
+++ index.js
@@ -0,0 +1,57 @@
+// // Example 1
+// // Can export a number (not common, but JS won't stop me!)
+// module.exports = 5;
+
+// // Example 2
+// // Can export an object (quite common!)
+// module.exports = { 
+//   firstName: "Lighthouse",
+//   lastName:  "Labs"
+// }
+
+// // Example 3
+// // Can export a function (we did this for all our functions)
+// module.exports = function() {
+//   return "developers, developers, developers!"; 
+// }
+
+// // Etc.
+
+const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const head = require('./head');
+const letterPositions = require('./letterPositions');
+const maps = require('./maps');
+const middle = require('./middle');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
+
+module.exports = {
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  flatten,
+  head,
+  letterPositions,
+  maps,
+  middle,
+  tail,
+  takeUntil,
+  without
+};
