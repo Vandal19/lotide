6ceea4981a5760361b5f51838dfd5a2e692207bf
Milton Vandol Goh <miltonvandol@gmail.com>
2022-04-13 18:49:42 -0700

    counting the number of specified item within an array

---
+++ countOnly.js
@@ -0,0 +1,51 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected} ✅✅✅`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected} 🛑🛑🛑`);
+  }
+};
+
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+
+const countOnly = (allItems, itemsToCount) => {
+  const results = {};
+
+  for(const item of allItems) {
+    if (itemsToCount[item]) {
+    if(results[item]) {
+      results[item] += 1
+    } else {
+      results[item] = 1
+    }
+    }
+  }
+  return results
+}
+
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { 
+  "Jason": true, 
+  "Karima": true, 
+  "Fang": true, 
+  "Agouhanna": false 
+});
+
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
